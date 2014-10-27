magne4000/quassel-webserver

commit 4ef6e889b8f1c1bc0edabfa3d922d13b48148e43

Working with last socket.io version

public/javascripts/views.js
@@ -129,8 +129,9 @@ Views._network = function(name) {
 	'<div class="network-channels clearfix" id="'+name+'-channels"></div>';
 };
 
-Views._buffer = function(bufferId, name) {
-	return '<div class="channel off" data-buffer-id="'+bufferId+'">' + 
+Views._buffer = function(bufferId, name, active) {
+	var activeclass = active?"on":"off";
+	return '<div class="channel '+activeclass+'" data-buffer-id="'+bufferId+'">' + 
 		'<span class="channel-icon"></span>' + 
 		'<span class="channel-name">'+name+'</span>' + 
 	'</div>';
@@ -207,10 +208,10 @@ Views.addNetwork = function(name) {
 	}
 };
 
-Views.addBuffer = function(networkname, bufferId, name) {
+Views.addBuffer = function(networkname, buffer) {
 	// Keep buffer ordered alphabetically
 	var channels = $('#'+networkname+'-channels .channel'), names = [], bufferIds = [], i = 0;
-	var html = Views._buffer(bufferId, name);
+	var html = Views._buffer(buffer.id, buffer.name, buffer.active);
 	var lowercaseName = name.toLowerCase(), spot = null;
 	channels.each(function(){
 		names.push($(this).text().toLowerCase());
@@ -243,8 +244,8 @@ Views.bufferMarkAsRead = function(bufferId) {
 	$(".channel[data-buffer-id="+bufferId+"]").removeClass("buffer-newmessage buffer-highlight");
 };
 
-Views.setStatusBuffer = function(networkname, bufferId) {
-	$('#network-'+networkname+' .network-name').attr("data-buffer-id", bufferId);
+Views.setStatusBuffer = function(networkname, buffer) {
+	$('#network-'+networkname+' .network-name').attr("data-buffer-id", buffer.id);
 };
 
 Views.getMessage = function(message) {
@@ -342,6 +343,10 @@ Views.hideNicks = function() {
     $("#hide-nicks").hide();
     $("#nick-pane").removeClass("col-md-2").addClass("col-md-0");
     $("#nick-pane .buffer-container").css("opacity", "0");
+};
+
+Views.removeBuffer = function(bufferId) {
+	$(".channel[data-buffer-id="+bufferId+"]").remove();
 };
 
 Views.hideBuffer = function(bufferId) {

public/javascripts/events.js
@@ -24,7 +24,7 @@ EventReceiver.prototype.on = function(event, callback) {
 		if (condition._check(event, args)) {
 			condition._call(event, args, callback);
 		} else {
-			console.log(event);
+			console.log(event, args);
 			condition._store(event, args, callback);
 		}
 	});
@@ -127,7 +127,9 @@ ConditionedEventReceiver.prototype._check = function(event, args) {
 				if (!thisone) break;
 			}
 		} else { thisone = false; }
-		if (thisone) nb--;
+		if (thisone) {
+			nb--;
+		}
 	}
 	return (nb <= 0);
 };
@@ -157,6 +159,7 @@ EventReceiver.definition = {
 	'buffer.remove': ["bufferId"],
 	'buffer.rename': ["bufferId", "bufferName"],
 	'buffer.activate': ["bufferId"],
+	'buffer.deactivate': ["bufferId"],
 	'buffer.message': ["bufferId", "messageId"],
 	'buffer.backlog': ["bufferId", "messageIds"],
 	'buffer.unhide': ["bufferId"],

public/javascripts/script.js
@@ -104,13 +104,15 @@ er.on('network.addbuffer', function(next, networkId, bufferId) {
 	console.log('addbuffer');
 	var network = networks.get(networkId);
 	var buffer = network.getBufferCollection().getBuffer(bufferId);
-	if (buffer.isStatusBuffer()) {
-		Views.setStatusBuffer(network.networkName, bufferId);
-	}
-	else {
-		Views.addBuffer(network.networkName, bufferId, buffer.name);
-	}
-	next();
+	reviver.afterReviving(buffer, function(obj) {
+		if (obj.isStatusBuffer()) {
+			Views.setStatusBuffer(network.networkName, obj);
+		}
+		else {
+			Views.addBuffer(network.networkName, obj);
+		}
+		next();
+	});
 }).after('network.init');
 
 er.on('change', function(next, networkId, change) {
@@ -136,7 +138,6 @@ er.on('buffer.backlog', function(next, bufferId, messageIds) {
 			Views.prependMessage(message);
 		}
 	}
-	Views.activateBuffer(bufferId);
 	next();
 }).after('network.addbuffer');
 
@@ -144,17 +145,17 @@ er.on('buffer.markerline', function(next, bufferId, messageId) {
 	console.log('buffer.markerline : ' + bufferId + ", " + messageId);
 	// TODO
 	next();
-}).after('buffer.backlog');
+}).after('network.addbuffer');
 
 er.on('buffer.lastseen', function(next, bufferId, messageId) {
 	console.log('buffer.lastseen : ' + bufferId + ", " + messageId);
 	messageId = parseInt(messageId, 10);
 	var buffer = networks.findBuffer(bufferId);
-	if (!buffer.isLast(messageId) && buffer.messages.has(messageId)) {
+	if (buffer !== null && !buffer.isLast(messageId) && buffer.messages.has(messageId)) {
 		Views.bufferHighlight(bufferId);
 	}
 	next();
-}).after('buffer.backlog');
+}).after('network.addbuffer');
 
 er.on('buffer.message', function(next, bufferId, messageId) {
 	console.log('buffer.message : ' + bufferId + ", " + messageId);
@@ -167,7 +168,12 @@ er.on('buffer.message', function(next, bufferId, messageId) {
 		Views.bufferHighlight(bufferId, message);
 	}
 	next();
-}).after('buffer.backlog');
+}).after('network.addbuffer');
+
+er.on('buffer.activate', function(next, bufferId) {
+	Views.activateBuffer(bufferId);
+	next();
+}).after('network.addbuffer');
 
 er.on('buffer.read', function(next, bufferId) {
 	console.log('buffer.read : ' + bufferId);
@@ -185,6 +191,11 @@ er.on('buffer.hidden', function(next, bufferId, type) {
 	next();
 }).after('network.addbuffer');
 
+er.on('buffer.remove', function(next, bufferId) {
+	Views.removeBuffer(bufferId);
+	next();
+}).after('network.addbuffer');
+
 er.on('channel.join', function(next, bufferId, nick) {
 	if (Views.isBufferShown(bufferId)) {
 		var buffer = networks.findBuffer(bufferId);
@@ -195,6 +206,7 @@ er.on('channel.join', function(next, bufferId, nick) {
 });
 
 er.on('user.part', function(next, networkId, nick, bufferName) {
+	console.log(networkId, nick, bufferName);
 	var network = networks.get(networkId);
 	var buffer = network.getBuffer(bufferName);
 	if (Views.isBufferShown(buffer.id)) {

commit 0c3f2d351368482f1a791f077146d263d1772370

Fix buffer sorting.
Bug introduced in 4ef6e889b8f1c1bc0edabfa3d922d13b48148e43

public/javascripts/views.js
@@ -212,7 +212,7 @@ Views.addBuffer = function(networkname, buffer) {
 	// Keep buffer ordered alphabetically
 	var channels = $('#'+networkname+'-channels .channel'), names = [], bufferIds = [], i = 0;
 	var html = Views._buffer(buffer.id, buffer.name, buffer.active);
-	var lowercaseName = name.toLowerCase(), spot = null;
+	var lowercaseName = buffer.name.toLowerCase(), spot = null;
 	channels.each(function(){
 		names.push($(this).text().toLowerCase());
 		bufferIds.push(parseInt($(this).attr("data-buffer-id"), 10));