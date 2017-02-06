264d4323e105736d76d1fbf275848ba4fbc35734
Yorick van Pelt <yorick@yorickvanpelt.nl>
2017-02-06 16:03:04 +0100

    Fix settings for electron

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const locals = {dev: true, settings};
 var j = require('electron-pug')({pretty: true}, locals);
 
 let mainWindow;
-// process.argv.push("-m")
-// process.argv.push("http")
-// require('./app.js')
+
+
+global.app_settings = {
+	version: 1,
+	settings: locals.settings.val.default,
+	themes: locals.settings.val.themes
+};
+
 
 app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 400, height: 360, darkTheme: true});
