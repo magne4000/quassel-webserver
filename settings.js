a18de213178e0b36a6b95cddc059c782e24ce04c
Sam Reed <sam@reedyboy.net>
2020-01-13 14:17:25 +0000

    Fix overhidden typo

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-    default: {  // Those can be overhidden in the browser
+    default: {  // Those can be overridden in the browser
         host: '',  // quasselcore host
         port: 4242,  // quasselcore port
         initialBacklogLimit: 20,  // Amount of backlogs to fetch per buffer on connection
@@ -12,10 +12,10 @@ module.exports = {
         highlightmode: 2  // Highlight mode: 1: None, 2: Current nick, 3: All nicks from identity
     },
     webserver: {
-        socket: false,  // Tells the webserver to listen for connections on a local socket. This should be a path. Can be overhidden by '--socket' argument
-        listen: null,  // Address on which to listen for connection, defaults to listening on all available IPs. Can be overhidden by '--listen' argument
-        port: null,  // Port on which to listen for connection, defaults to 64080 for http mode, 64443 for https. Can be overhidden by '--port' argument
-        mode: null  // can be 'http' or 'https', defaults to 'https'. Can be overhidden by '--mode' argument
+        socket: false,  // Tells the webserver to listen for connections on a local socket. This should be a path. Can be overridden by '--socket' argument
+        listen: null,  // Address on which to listen for connection, defaults to listening on all available IPs. Can be overridden by '--listen' argument
+        port: null,  // Port on which to listen for connection, defaults to 64080 for http mode, 64443 for https. Can be overridden by '--port' argument
+        mode: null  // can be 'http' or 'https', defaults to 'https'. Can be overridden by '--mode' argument
     },
     themes: ['default', 'darksolarized'],  // Available themes
     forcedefault: false,  // Will force default host and port to be used if true, and will hide the corresponding fields in the UI.
